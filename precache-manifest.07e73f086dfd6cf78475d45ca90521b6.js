305ee489f56f32ccfd3cc40e9a2c6731b20e1fa7
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-10-16 17:22:16 +0530

    Updates

---
+++ precache-manifest.07e73f086dfd6cf78475d45ca90521b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c446929bb4df57a82e00642a4dc8d07",
+    "url": "/Konvo-basic_drawing/index.html"
+  },
+  {
+    "revision": "57ed19947412af4528a6",
+    "url": "/Konvo-basic_drawing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d0c249d5374e73dc05d",
+    "url": "/Konvo-basic_drawing/static/js/2.11763297.chunk.js"
+  },
+  {
+    "revision": "367d0bc1f4ce0d0b4e75cfad70c99eb5",
+    "url": "/Konvo-basic_drawing/static/js/2.11763297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ed19947412af4528a6",
+    "url": "/Konvo-basic_drawing/static/js/main.f2c88e2d.chunk.js"
+  },
+  {
+    "revision": "a262ea53118ee05b6c43",
+    "url": "/Konvo-basic_drawing/static/js/runtime-main.b0d2d398.js"
+  }
+]);
