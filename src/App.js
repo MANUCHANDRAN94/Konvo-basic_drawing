a2fed808bd65657de7d9328176d7c35bc87a5f68
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-10-19 21:06:01 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Stage, Layer, Rect, Text, Circle } from 'react-konva';
 
 const App = () => {
 
-  const [shape, setShape] = React.useState({
-    rect: {
+  const [shape, setShape] = React.useState([
+    {
       name: 'rect',
       x: 100,
       y: 50,
@@ -12,75 +12,72 @@ const App = () => {
       height: 100,
       stroke: 'black',
       dragging: false,
-      zIdx: 0
     },
-    circ: {
+    {
       name: 'circ',
       x: 300,
       y: 100,
       radius: 50,
       stroke: 'green',
       dragging: false,
-      zIdx: 0
     }
-  });
+  ]);
 
   const handleDragStart = (e) => {
-    let shapeName = e.target.name()
-    setShape((prev) => {
-      return {
-        ...prev,
-        [shapeName]: {
-          ...prev[shapeName],
-          dragging: true,
-          zIdx: (prev[shapeName].zIdx + 1)
+    let shapeId = e.target.name()
+    setShape(prev =>
+      prev.map(item => {
+        return {
+          ...item,
+          dragging: (item.name === shapeId)
         }
-      }
-    })
+      }))
   };
   const handleDragEnd = (e) => {
-    let shapeName = e.target.name()
-    setShape((prev) => {
-      return {
-        ...prev,
-        [shapeName]: {
-          ...prev[shapeName],
-          dragging: false
+    setShape(prev =>
+      prev.map(item => {
+        return {
+          ...item,
+          dragging: false,
         }
-      }
-    })
+      }))
   }
   return (
     <Stage width={window.innerWidth} height={window.innerHeight} >
       <Layer>
-        {/* console.log({shape.circ.dragging}) */}
-        <Rect
-          name={shape.rect.name}
-          x={shape.rect.x}
-          y={shape.rect.y}
-          width={shape.rect.width}
-          height={shape.rect.height}
-          stroke={shape.rect.stroke}
-          isDragging={shape.rect.dragging}
-          draggable
-          onDragStart={handleDragStart}
-          onDragEnd={handleDragEnd}
-          fill={shape.rect.dragging ? 'red' : null}
-          zIndex={shape.rect.zIdx}
-        />
-        <Circle x={shape.circ.x}
-          name={shape.circ.name}
-          y={shape.circ.y}
-          radius={shape.circ.radius}
-          stroke={shape.circ.stroke}
-          isDragging={shape.circ.dragging}
-          draggable
-          onDragStart={handleDragStart}
-          onDragEnd={handleDragEnd}
-          fill={shape.circ.dragging ? 'yellow' : null}
-          zIndex={shape.circ.zIdx}
-        />
-        <Text text="Konvo try" fontSize={25} fill='red' textDecoration='underline' padding='20' />
+        {shape.map((item, i) =>
+          [item.name === 'rect' && <Rect
+            key={i}
+            id={i}
+            name={item.name}
+            x={item.x}
+            y={item.y}
+            width={item.width}
+            height={item.height}
+            stroke={item.stroke}
+            isDragging={item.dragging}
+            draggable
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            fill={item.dragging ? 'red' : null}
+          />,
+          item.name === 'circ' && <Circle x={item.x}
+            key={i}
+            id={i}
+            name={item.name}
+            y={item.y}
+            radius={item.radius}
+            stroke={item.stroke}
+            isDragging={item.dragging}
+            draggable
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            fill={item.dragging ? 'yellow' : null}
+          />]
+        )}
+
+
+        <Text text="Konvo try" fontSize={25} fill='red' textDecoration='underline' />
 
       </Layer>
     </Stage>
